78b8da26671ba91cd7b318dfd6825dfd01e5bb8e
Justin Wetherell <justin.wetherell@nasdaq.com>
2017-04-03 11:49:25 -0400

    Changed graph DFS to use a Graph object instead of int[]

--- src/com/jwetherell/algorithms/graph/DepthFirstTraversal.java
+++ src/com/jwetherell/algorithms/graph/DepthFirstTraversal.java
@@ -1,41 +1,82 @@
+package com.jwetherell.algorithms.graph;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Stack;
 
-//implemented Depth First Travesal in given "Directed graph" (Adjacany matrix)
-class DepthFirstTraversal {
+import com.jwetherell.algorithms.data_structures.Graph;
+import com.jwetherell.algorithms.data_structures.Graph.Edge;
+import com.jwetherell.algorithms.data_structures.Graph.Vertex;
 
-    public static int[] arr;
-    public static int k = 0;
+/* Depth First Travesal in given "Directed graph" (Adjacany matrix) */
+public class DepthFirstTraversal {
 
-    public static void depthFirstTraversal(int[][] a, int[] visited,int source){
-        for (int i = 0; i < visited.length; i++) {
-            visited[i] = -1;
+    @SuppressWarnings("unchecked")
+    public static final <T extends Comparable<T>> Graph.Vertex<T>[] depthFirstTraversal(Graph<T> graph, Graph.Vertex<T> source) {
+        // use for look-up via index
+        final ArrayList<Vertex<T>> vertices = new ArrayList<Vertex<T>>();
+        vertices.addAll(graph.getVertices());
+
+        // used for look-up via vertex
+        final int n = vertices.size();
+        final Map<Vertex<T>,Integer> vertexToIndex = new HashMap<Vertex<T>,Integer>();
+        for (int i=0; i<n; i++) {
+            final Vertex<T> v = vertices.get(i);
+            vertexToIndex.put(v,i);
         }
-        Stack<Integer>stack = new Stack();
-        int element = source;       
-        int i = source;
-        int n = visited.length - 1;
+
+        // adjacency matrix
+        final byte[][] adj = new byte[n][n];
+        for (int i=0; i<n; i++) {
+            final Vertex<T> v = vertices.get(i);
+            final int idx = vertexToIndex.get(v);
+            final byte[] array = new byte[n];
+            adj[idx] = array;
+            final List<Edge<T>> edges = v.getEdges();
+            for (Edge<T> e : edges)
+                array[vertexToIndex.get(e.getToVertex())] = 1;
+        }
+
+        // visited array
+        final byte[] visited = new byte[n];
+        for (int i = 0; i < visited.length; i++)
+            visited[i] = -1;
+
+        // for holding results
+        final Graph.Vertex<T>[] arr = new Graph.Vertex[n];
+
+        // start at the source
+        Vertex<T> element = source;
+        int c = 0;
+        int i = vertexToIndex.get(element);
+        visited[i] = 1;   
+        int k = 0;
         arr[k] = element;
         k++;
-        visited[source] = 1;        
+
+        final Stack<Vertex<T>> stack = new Stack<Vertex<T>>();
         stack.push(source);
- 
         while (!stack.isEmpty()) {    
             element = stack.peek();
-            i = element;    
-            while (i <= n) {
-                if (a[element][i] == 1 && visited[i] == -1) {
-                    stack.push(i);
+            c = vertexToIndex.get(element);
+            i = 0;    
+            while (i < n) {
+                if (adj[c][i] == 1 && visited[i] == -1) {
+                    stack.push(element);
                     visited[i] = 1;
-                    element = i;
-                    i = 1;
+                    element = vertices.get(i);
                     arr[k] = element;
-                    k++;                
+                    k++;
+                    c = vertexToIndex.get(element);
+                    i = 0;
                     continue;
                 }
                 i++;
             }
             stack.pop();    
         }
+        return arr;
     }
 }
-
